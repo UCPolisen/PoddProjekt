90e9bfcea6df5f63df7317b05623a4f72dadc770
UCPolisen <168416050+UCPolisen@users.noreply.github.com>
2024-10-15 13:05:11 +0200

    Update Form1.Designer.cs

--- PoddProjekt/Form1.Designer.cs
+++ PoddProjekt/Form1.Designer.cs
@@ -28,19 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
+            splitContainer1 = new System.Windows.Forms.SplitContainer();
+            button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.SuspendLayout();
             SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            splitContainer1.Location = new System.Drawing.Point(0, 0);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(button1);
+            splitContainer1.Size = new System.Drawing.Size(1611, 1101);
+            splitContainer1.SplitterDistance = 537;
+            splitContainer1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new System.Drawing.Point(105, 143);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(150, 46);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            ClientSize = new System.Drawing.Size(803, 516);
+            ClientSize = new System.Drawing.Size(1611, 1101);
+            Controls.Add(splitContainer1);
+            Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             Name = "Form1";
             Text = "PoddApp";
             Load += Form1_Load;
+            splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button button1;
     }
 }
